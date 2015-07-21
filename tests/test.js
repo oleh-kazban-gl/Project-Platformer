04be09d0fbda1c36a94c6fcb7ceb474a158245e4
Oleh Kazban <oleh.kazban@globallogic.com>
2015-07-21 16:45:59 +0300

    Update: Tests for Levels

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,5 @@
+require("amd-loader");
+
 var expect = require('expect.js'),
   sinon = require('sinon'),
   levels = require('./../js/app/Levels/Levels')
@@ -6,8 +8,20 @@ var expect = require('expect.js'),
 describe('Platformer', function () {
 
   describe('Levels', function () {
-    it('Should sort given array of numbers and pass in ascending ordering (with ascending comparator: a < b)', function () {
-      expect().to.equal(true);
+    it('Levels should be an Array', function () {
+      expect(Object.prototype.toString.call(levels).toUpperCase()).to.equal('[OBJECT ARRAY]');
+    });
+
+    it('Should be non-zero length', function() {
+      expect(levels.length).to.be.greaterThan(0);
+    });
+
+    it('Should consists of Arrays of \"Levels\"', function() {
+      expect(Object.prototype.toString.call(levels[0]).toUpperCase()).to.equal('[OBJECT ARRAY]');
+    });
+
+    it('Level should be an Array of Strings', function() {
+      expect(levels[0][0]).to.be.a('string');
     });
   });
 
