1d22034271d44b89a402b12afd858d66dce65037
Oleh Kazban <oleh.kazban@globallogic.com>
2015-07-27 13:54:45 +0300

    Update: zero-level behavior

--- js/app/Helpers/runGame.js
+++ js/app/Helpers/runGame.js
@@ -5,64 +5,63 @@
  */
 
 define(function (require) {
-    'use strict';
+  'use strict';
 
-    var runLevel = require('./runLevel');
-    var Level = require('../World/Level');
-    var soundEngine = require('../Engine/Audio/EngineAudio');
+  var runLevel = require('./runLevel');
+  var Level = require('../World/Level');
+  var soundEngine = require('../Engine/Audio/EngineAudio');
 
-    var runGame = {
-        run: function (plans, Display) {
+  var runGame = {
+    run: function (plans, Display) {
 
-            soundEngine.soundInit();
+      soundEngine.soundInit();
 
-            function startLevel(n) {
-                var level = new Level(plans[n]);
+      function startLevel(n) {
+        var level = new Level(plans[n]);
 
-                runGame.coins = level.coins;
-                runGame.level = level;
-                level.levelId = n;
+        runGame.coins = level.coins;
+        runGame.level = level;
+        level.levelId = n;
 
-                runLevel(level, Display, function (status) {
-                    if (status === 'lost' && runGame.lives > 0) {
-                      console.log('Level: ' + n);
+        runLevel(level, Display, function (status) {
+          if (status === 'lost' && runGame.lives > 0) {
 
-                        // If player contrives to die at zero level, restoring
-                        if (n === 0) {
-                          startLevel(0);
-                        } else {
-                          runGame.lives -= 1; // Player died, for respawn spent 1 life
-                          console.log('Level: #' + n + ', lives: ' + runGame.lives);
+            // If zero level, restoring lives
+            if (n === 0) {
+              runGame.lives = 3;
+              startLevel(0);
+            } else {
+              runGame.lives -= 1; // Player died, for respawn spent 1 life
+              console.log('Level: #' + n + ', lives: ' + runGame.lives);
 
-                          startLevel(n);
-                        }
-
-                    } else if (runGame.lives < 0) {
-                        soundEngine.soundPlay('gameOver');
-                        runGame.lives = 3;
-
-                        startLevel(0);
-                    } else if (n < plans.length - 1) {
-                        runGame.lives += 1; // Reward for finishing level
-                        n += 1;
-
-                        soundEngine.soundPlay('nextLevel');
-
-                        console.log('Level: #' + n + ', lives: ' + runGame.lives);
-
-                        startLevel(n);
-                    } else {
-                        console.log('You win!');
-                    }
-                });
+              startLevel(n);
             }
+          } else if (runGame.lives < 0) {
+            soundEngine.soundPlay('gameOver');
+            runGame.lives = 3;
 
             startLevel(0);
-        },
-        lives: 3,
-        level: {}
-    };
+          } else if (n < plans.length - 1) {
+            runGame.lives += 1; // Reward for finishing level
+            n += 1;
 
-    return runGame;
+            soundEngine.soundPlay('nextLevel');
+
+            console.log('Level: #' + n + ', lives: ' + runGame.lives);
+
+            startLevel(n);
+          } else {
+            console.log('You win!');
+          }
+        });
+      }
+
+      startLevel(0);
+    },
+    lives: 3,
+    level: {}
+  };
+
+  return runGame;
 });
 
